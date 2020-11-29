27f3f61451423115b3453dd916a23ff682a076de
pavanpw <pavan.kalyan@salussg.com>
2020-11-30 00:31:34 +0530

    adding form validation

--- script.js
+++ script.js
@@ -1,92 +1,22 @@
-var obj = [
-  {
-    name: "Pavan",
-    mobile: 7013224891,
-    age: 23,
-  },
-  {
-    name: "kalyan",
-    mobile: 7075023250,
-    age: 26,
-  },
-  {
-    name: "Ram",
-    mobile: 09283982638,
-    age: 29,
-  }
-];
-
-var responce = {
-    "page": 2,
-    "per_page": 6,
-    "total": 12,
-    "total_pages": 2,
-    "data": [
-        {
-            "id": 7,
-            "email": "michael.lawson@reqres.in",
-            "first_name": "Michael",
-            "last_name": "Lawson",
-            "avatar": "https://reqres.in/img/faces/7-image.jpg"
-        },
-        {
-            "id": 8,
-            "email": "lindsay.ferguson@reqres.in",
-            "first_name": "Lindsay",
-            "last_name": "Ferguson",
-            "avatar": "https://reqres.in/img/faces/8-image.jpg"
-        },
-        {
-            "id": 9,
-            "email": "tobias.funke@reqres.in",
-            "first_name": "Tobias",
-            "last_name": "Funke",
-            "avatar": "https://reqres.in/img/faces/9-image.jpg"
-        },
-        {
-            "id": 10,
-            "email": "byron.fields@reqres.in",
-            "first_name": "Byron",
-            "last_name": "Fields",
-            "avatar": "https://reqres.in/img/faces/10-image.jpg"
-        },
-        {
-            "id": 11,
-            "email": "george.edwards@reqres.in",
-            "first_name": "George",
-            "last_name": "Edwards",
-            "avatar": "https://reqres.in/img/faces/11-image.jpg"
-        },
-        {
-            "id": 12,
-            "email": "rachel.howell@reqres.in",
-            "first_name": "Rachel",
-            "last_name": "Howell",
-            "avatar": "https://reqres.in/img/faces/12-image.jpg"
+$(document).ready(function(){
+    $(".myform").submit(function(e){
+        e.preventDefault();
+        var email = $("#email").val();
+        var pass = $("#pwd").val();
+        // writing Regex for email
+        var email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+        // writing Regex for password
+        var pass_regex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+        // checking email is valid or not
+        if(email_regex.test(email)){
+            // checking password is strong or not
+            if(pass_regex.test(pass)){
+                console.log(email+" "+pass);
+            }else{
+                console.log("error in password: password not storng");
+            }
+        }else{
+            console.log("error in email");
         }
-    ],
-    "support": {
-        "url": "https://reqres.in/#support-heading",
-        "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
-    }
-}
-
-var card_html = "";
-var mydata = responce.data;
-
-mydata.map(function(e){
-    
-    card_html+= `<div class="card">
-    <div class="card_header">
-        <h1>`+e.first_name+`</h1>
-    </div>
-    <div class="card_body">
-        <p>Mobile: `+e.email+`</p>
-        <p>Age: `+e.id+`</p>
-    </div>
-</div>`;
+    }); 
 });
-
-document.getElementById("demo").innerHTML = card_html;
-
-
